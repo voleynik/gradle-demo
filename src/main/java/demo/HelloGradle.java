70e84d43995773e0e54d077bc91ff5863564953f
vo <Kiev1kiev@>
2018-02-19 19:31:27 +0200

    Initial commit

---
+++ src/main/java/demo/HelloGradle.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.joda.time.LocalTime;
+
+class HelloGradle{
+	
+	public static void main(String... args){
+		
+		LocalTime time = new LocalTime();
+		System.out.println("Hello Gradle - local time: " + time);
+		
+	}
+}
